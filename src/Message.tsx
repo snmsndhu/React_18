ef4239a0dd13e35bed89b19f8ad3a2a7c0dbbe82
Sandeep Singh <sandeepsingh@Sandeeps-Mac-mini.local>
2023-12-07 08:05:48 -0500

    test

--- src/Message.tsx
+++ src/Message.tsx
@@ -2,7 +2,7 @@ function Message() {
     const name = ''
     if(name)
     return <h1>Hello {name}</h1>;
-    return <h1>Hello world</h1>
+    return <h1>Hello world!</h1>
 }
 
 export default Message;
