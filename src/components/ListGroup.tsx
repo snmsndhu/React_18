466235b985705867ee9466cdc88c60341b016095
Sandeep Singh <snmsndhu47@gmail.com>
2023-12-07 08:44:03 -0500

    Building the list component

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,11 @@
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London"];
+  let items = ["New York", "San Francisco", "Tokyo", "London"];
+  items = [];
+
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No Items found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
