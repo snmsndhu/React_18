d029da8df299ddce1b6c1107b501fa44514c09d2
Sandeep Singh <snmsndhu47@gmail.com>
2023-12-08 10:49:15 -0500

    Adding props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  
-  return <div><ListGroup/></div>;
+  let items = ["New York", "San Francisco", "Tokyo", "London"];
+
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities" />
+    </div>
+  );
 }
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London"];
-
+interface Props {
+  items: string[];
+  heading: string;
+}
+function ListGroup({ items, heading }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No Items found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
