7cba0e4737562241a8f1612885fc471b7e03c21a
Sandeep Singh <snmsndhu47@gmail.com>
2023-12-08 10:55:44 -0500

    Adding props

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,17 @@ import ListGroup from "./components/ListGroup";
 
 function App() {
   let items = ["New York", "San Francisco", "Tokyo", "London"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
 
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
