da8a7ac531dde3f1671ac2ea467e3401cf834ce5
Sandeep Singh <snmsndhu47@gmail.com>
2023-12-08 11:09:03 -0500

    Building the alert component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>Hello World</Alert>
     </div>
   );
 }
