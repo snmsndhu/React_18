2ba9e0bcabbbb62e8f99148fd5f6f6ec9cf3ed4b
Sandeep Singh <snmsndhu47@gmail.com>
2023-12-14 11:50:22 -0500

    creating the Expandable Text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import { useState } from "react";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
-  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
   return (
     <div>
-      <NavBar cardItemsCount={cartItems.length} />
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
+      <ExpandableText maxChars={10}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+        quaerat dolore cupiditate expedita iure beatae, error reprehenderit,
+        dolorum, asperiores atque tempore? Nesciunt itaque cumque quaerat! Esse
+        nisi obcaecati tempore accusamus molestiae sunt alias libero, aut,
+        officiis laborum voluptatibus unde similique deserunt blanditiis cum,
+        architecto ratione totam nemo. Enim rerum molestiae voluptatem itaque
+        similique ipsum earum nam. Consequuntur, omnis labore rerum dolorem
+        quasi vel ipsum repellat aperiam deserunt provident ex ullam quam
+        possimus voluptatibus suscipit est minima error neque officiis. Ullam
+        maxime vero odio odit quae excepturi, porro aspernatur, consequuntur,
+        sequi molestias perferendis beatae. Quaerat ut est aspernatur ipsam!
+        Iusto, delectus!
+      </ExpandableText>
     </div>
   );
 }
