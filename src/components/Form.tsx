80727d8b8df79c955c80b0b746f56897cad610a1
Sandeep Singh <snmsndhu47@gmail.com>
2023-12-15 12:30:52 -0500

    building the form component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import { FormEvent } from "react";
 
 const Form = () => {
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log("Submitted");
+  };
   return (
-    <form className="mb-3">
-      <label htmlFor="name" className="form-label">
-        Name
-      </label>
-      <input id="name" type="text" className="form-control" />
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input id="name" type="text" className="form-control" />
+      </div>
+
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
