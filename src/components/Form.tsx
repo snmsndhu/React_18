8b8d1857de2d6591d4b5510151281c9c86ab0de9
Sandeep Singh <snmsndhu47@gmail.com>
2023-12-15 12:24:38 -0500

    building the form component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,12 +2,21 @@ import React from "react";
 
 const Form = () => {
   return (
-    <div className="mb-3">
+    <form className="mb-3">
       <label htmlFor="name" className="form-label">
         Name
       </label>
       <input id="name" type="text" className="form-control" />
-    </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input id="age" type="number" className="form-control" />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
   );
 };
 
