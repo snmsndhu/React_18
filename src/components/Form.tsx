ef7f6303181e4fc62c26ff6c2b03f333d44fb866
Sandeep Singh <snmsndhu47@gmail.com>
2023-12-20 11:21:17 -0500

    useRef

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,11 +3,16 @@ import { FormEvent, useRef } from "react";
 const Form = () => {
   const nameRef = useRef<HTMLInputElement>(null);
   const ageRef = useRef<HTMLInputElement>(null);
+  const person = {
+    name: "",
+    age: 0,
+  };
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    if (nameRef.current !== null) console.log(nameRef.current.value);
-    if (ageRef.current !== null) console.log(ageRef.current.value);
+    if (nameRef.current !== null) person.name = nameRef.current.value;
+    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+    console.log(person);
   };
   return (
     <form onSubmit={handleSubmit}>
