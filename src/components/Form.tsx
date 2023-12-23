ced420a771bc922303ebe95065bf08597445284d
Sandeep Singh <snmsndhu47@gmail.com>
2023-12-23 10:09:33 -0500

    disabling the butoon when the form is not valid

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -44,7 +44,7 @@ const Form = () => {
         />
         {errors.age && <p className="text-danger">{errors.age.message}</p>}
       </div>
-      <button className="btn btn-primary" type="submit">
+      <button disabled={!isValid} className="btn btn-primary" type="submit">
         Submit
       </button>
     </form>
